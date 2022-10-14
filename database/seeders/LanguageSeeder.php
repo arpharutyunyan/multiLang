61a3c78ae9a4a29f6911e6b098cce442979acafa
arpharutyunyan <arpine.harutyunyana@gmail.com>
2022-10-14 14:46:48 +0400

    language table with seeder

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -2,19 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class LanguageSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(LanguageSeeder::class);
+        Language::insert([
+            ['code' => 'en'],
+            ['code' => 'hy']
+        ]);
     }
 }
